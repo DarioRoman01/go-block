36793ecdd22c1b902ede86556b335b41bfde4987
Haizza1 <darioroman2001@gmail.com>
2021-05-22 08:09:09 -0400

    refactoring cli app to his own package.

---
+++ cli/cli.go
@@ -0,0 +1,151 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+
+	"github.com/Haizza1/go-block/blockchain"
+)
+
+type CommandLine struct{}
+
+// print usage will print the cli usage
+func (cli *CommandLine) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("	getbalance -address <ADDRESS> - get the balance for the given address")
+	fmt.Println(" 	createBlockchain -address <ADDRESS> create a blockchain with the given address")
+	fmt.Println(" 	printchain - Prints the blocks in the Blockchain")
+	fmt.Println(" 	send -from <FROM> -to <TO> -amount <AMOUNT> - Send Send amount of coins")
+}
+
+// validateArgs will check if the given args
+func (cli *CommandLine) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		runtime.Goexit()
+	}
+}
+
+// printChain will print all the blocks in the blockchain
+func (cli *CommandLine) printChain() {
+	chain := blockchain.ContinueBlockChain("")
+	defer chain.Database.Close()
+	iter := chain.Iterator()
+
+	for {
+		block := iter.Next()
+
+		fmt.Printf("Previos Hash: %x\n", block.PrevHash)
+		fmt.Printf("Block Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(block.PrevHash) == 0 {
+			break
+		}
+	}
+}
+
+// create blockchain will create a new blockchain instance with the given address
+func (cli *CommandLine) createBLockChain(address string) {
+	chain := blockchain.InitBLockChain(address)
+	chain.Database.Close()
+	fmt.Println("Finished!")
+}
+
+func (cli *CommandLine) getBalance(address string) {
+	chain := blockchain.ContinueBlockChain(address)
+	defer chain.Database.Close()
+
+	balance := 0
+	unspentTxs := chain.FindUTXO(address)
+
+	for _, out := range unspentTxs {
+		balance += out.Value
+	}
+
+	fmt.Printf("Balance of %s: %d\n", address, balance)
+}
+
+func (cli *CommandLine) send(from, to string, amount int) {
+	chain := blockchain.ContinueBlockChain(from)
+	defer chain.Database.Close()
+
+	tx := blockchain.NewTransaction(from, to, amount, chain)
+	chain.AddBlock([]*blockchain.Transaction{tx})
+	fmt.Println("Success!")
+}
+
+// Run will start the comman line app and validate the args
+func (cli *CommandLine) Run() {
+	cli.validateArgs()
+
+	getBalanceCmd := flag.NewFlagSet("getbalance", flag.ExitOnError)
+	createBLockchainCmd := flag.NewFlagSet("createBlockchain", flag.ExitOnError)
+	sendCmd := flag.NewFlagSet("send", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+
+	getBalanceAddress := getBalanceCmd.String("address", "", "The address to get balance for")
+	createBLockChainAddress := createBLockchainCmd.String("address", "", "The address to send genesis block reward to")
+	sendFrom := sendCmd.String("from", "", "Source wallet address")
+	sendTo := sendCmd.String("to", "", "Destination wallet address")
+	sendAmount := sendCmd.Int("amount", 0, "Amount to send")
+
+	switch os.Args[1] {
+	case "getbalance":
+		err := getBalanceCmd.Parse(os.Args[2:])
+		blockchain.CheckError(err)
+
+	case "send":
+		err := sendCmd.Parse(os.Args[2:])
+		blockchain.CheckError(err)
+
+	case "printchain":
+		err := sendCmd.Parse(os.Args[2:])
+		blockchain.CheckError(err)
+
+	case "createBlockchain":
+		err := createBLockchainCmd.Parse(os.Args[2:])
+		blockchain.CheckError(err)
+
+	default:
+		cli.printUsage()
+		runtime.Goexit()
+	}
+
+	if getBalanceCmd.Parsed() {
+		if *getBalanceAddress == "" {
+			cli.printUsage()
+			runtime.Goexit()
+		}
+
+		cli.getBalance(*getBalanceAddress)
+	}
+
+	if sendCmd.Parsed() {
+		if *sendFrom == "" || *sendTo == "" || *sendAmount <= 0 {
+			cli.printUsage()
+			runtime.Goexit()
+		}
+
+		cli.send(*sendFrom, *sendTo, *sendAmount)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+
+	if createBLockchainCmd.Parsed() {
+		if *createBLockChainAddress == "" {
+			cli.printUsage()
+			runtime.Goexit()
+		}
+
+		cli.createBLockChain(*createBLockChainAddress)
+	}
+}
